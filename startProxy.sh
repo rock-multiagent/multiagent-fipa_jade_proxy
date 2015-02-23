de31d09a7fadbd1d58e852727b67e218046f5446
Thomas Roehr <thomas.roehr@dfki.de>
2015-02-23 12:29:07 +0100

    Update start script

--- startProxy.sh
+++ startProxy.sh
@@ -1,1 +1,5 @@
-java -jar ./target/fipa_services-0.0.1-jar-with-dependencies.jar -gui -agents echoAgent:multiagent.fipa_services.example.EchoAgent;tcpMtpAgent:multiagent.fipa_services.TcpMtpAgent
+java -jar ./target/fipa_services-0.0.1-jar-with-dependencies.jar -gui -agents tcpMtpAgent:multiagent.fipa_services.TcpMtpAgent;echoAgent:multiagent.fipa_services.example.EchoAgent
+# Note: starting multiple agents seems not to work via -agents: start from GUI
+# open nodes of AgentPlatforms-><IP>->MainContainer
+# right-click on MainContainer -> Start New Agent, browse and select the EchoAgent and give it a
+# proper name
