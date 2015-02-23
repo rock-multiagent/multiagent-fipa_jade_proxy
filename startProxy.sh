56fe8f120142e385d2238202ddb470185bb20ccd
Thomas Roehr <thomas.roehr@dfki.de>
2015-02-23 12:24:32 +0100

    Adapt start script

--- startProxy.sh
+++ startProxy.sh
@@ -1,1 +1,1 @@
-java -jar ./target/fipa_services-0.0.1-jar-with-dependencies.jar -gui -agents tcpMtpAgent:multiagent.fipa_services.TcpMtpAgent
+java -jar ./target/fipa_services-0.0.1-jar-with-dependencies.jar -gui -agents echoAgent:multiagent.fipa_services.example.EchoAgent;tcpMtpAgent:multiagent.fipa_services.TcpMtpAgent
